0e328f01d6e24a9d58347ae8192e0c5eab7660b9
JV <114490572+binJV@users.noreply.github.com>
2023-02-28 22:23:52 +0530

    Update Vqe_BeH2.py

--- Vqe_BeH2.py
+++ Vqe_BeH2.py
@@ -1,53 +1,35 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
 import pennylane as qml
 from pennylane import numpy as np
 from pennylane import qchem
 import matplotlib.pyplot as plt
 
 
-# In[2]:
-
-
 symbols= ["H","Be","H"]
 coordinates = np.array([[ 0.69683,   1.16085,   0.],[-0.49317, 1.16085,0. ],[-1.68317,   1.16085,   0.]])
-#qchem library computes our required no of Qubits and the hamiltonian of molecule
+
 Ham, qubits= qchem.molecular_hamiltonian(symbols,coordinates,charge=0)
 print(qubits,Ham)
-
-
-# In[3]:
 
 
 hf = qchem.hf_state(electrons=6, orbitals = qubits)
 print(hf)
 
 
-# In[4]:
-
-
 num_wires =qubits
 dev = qml.device("default.qubit", wires = num_wires)
 @qml.qnode(dev)
-def exp_energy(state):   #the state is representates as a jordan wigner representation
+def exp_energy(state):  
     qml.BasisState(np.array(state), wires= range(num_wires)) #prepare state
     return qml.expval(Ham)
 
 
-# In[5]:
-
-
 exp_energy(hf)
 
+# different dependencies on theta or parameter of various double excitations reveal the contribution of other excited states or the pair correlation energy terms  
+# that help in reaching the ground State energy. 
+# It can also be seen via parameter graph that the the contribution with active space state has most contribution. 
+# Choosing a better ansatz based on pair correlation energy help in reducing the total energy
 
-# In[6]:
-
-
-# Step 2: Prepare a trial Ground state as the above energe is not ground state energy
 def ansatz(params):
     qml.BasisState(hf,wires=range(num_wires))
     qml.DoubleExcitation(params[0], wires=[0,1,6,7])
@@ -66,12 +48,6 @@ def ansatz(params):
     
     
 
-
-# In[12]:
-
-
-#Minimizing the Expectation Value of the Hamiltonian
-#build a circuit that returns the expectation value of hamiltonian and returns a ground state  that is a cost function
 @qml.qnode(dev)
 def cost_function(params):
     ansatz(params)
@@ -79,13 +55,7 @@ def cost_function(params):
 
 
 
-# In[19]:
-
-
-# minimize the cost function or the expectation value what we mean is that based on theta of double excitation states our
-#probabilities of the created state is dependent on these theta so a particular amount of contribution will give minimum energy.
-
-theta = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],requires_grad = True)  # start at theta = -2 and then use opt to reach minima with some stepsize by some iteration
+theta = np.array([0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0],requires_grad = True) 
 opt = qml.GradientDescentOptimizer(stepsize=0.1)
 energy = [cost_function(theta)]
 angle = [theta]
@@ -98,11 +68,8 @@ for n in range (max_iterations):
     angle.append(theta)
     
     
-    if n%2==0:    #prints theta and cost after every 10 iterations
+    if n%2==0:
         print(f"Step = {n}, Energy = {energy[-1]:.8f} Ha")
-
-
-# In[111]:
 
 
 new=np.zeros(max_iterations+1)
@@ -110,40 +77,15 @@ print(angle[3][3])
 for i in range (max_iterations+1):
     new[i]=angle[i][4]
 plt.plot(new,energy)
-#plt.plot(angle,energy)
-
-
-# In[40]:
 
 
 print(f"Final Ground energy:  {energy[-1]:.8f} Ha")
 print(f"Final angle Parameters: = {theta[0]:.8f} {theta[1]:.8f} {theta[2]:.8f} {theta[3]:.8f} {theta[4]:.8f} {theta[5]:.8f} {theta[6]:.8f} {theta[7]:.8f} {theta[8]:.8f} {theta[9]:.8f} {theta[10]:.8f} {theta[11]:.8f} ")
 
 
-# In[28]:
-
-
-# what is actual ground state? ask from pennylane
 @qml.qnode(dev)
 def ground_state(params):   
     ansatz(params)
     return qml.state()
 
-
-# In[29]:
-
-
 ground_state(theta)
-
-
-# In[ ]:
-
-
-
-
-
-# In[ ]:
-
-
-
-
